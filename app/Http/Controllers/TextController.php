4d05cbc48c8ee7de3077a4984297a8dea8e037bf
echoterina <kate.makarova@hotmail.com>
2020-09-03 09:43:14 -0400

    query

--- app/Http/Controllers/TextController.php
+++ app/Http/Controllers/TextController.php
@@ -11,25 +11,22 @@ class TextController extends Controller
 {
     public function index()
     {
-        $texts = Text::all()->toArray();
+        $data = Db::table('texts')
+            ->join('text_user', 'text_user.text_id = texts.id')
+            ->where('text_user.id', Auth::user()->id)
+            ->select(
+                'texts.text_title',
+                'texts_site_name',
+                'texts.direct_link',
+            'texts.publication_date',
+            'texts_total_words',
+            'text_user.percentage',
+            'text_user.known_words')
+            ->orderBy('text_user.percentage', 'desc')
+            ->get();
 
-        foreach($texts as &$text)
-        {
-            //todo: optimize the request
-            $known = DB::table('word_text')
-            ->join('word_user', 'word_text.word', '=', 'word_user.word')
-            ->where('word_text.text_id', $text['id'])
-            ->where('word_user.user_id', Auth::user()->id)
-            ->count('*');
 
-            $text['known_words'] = $known;
-            $text['percentage'] = floor($known/$text['total_words']*100);
-        }
 
-        usort($texts, function($a, $b) {
-            return $a['percentage'] > $b['percentage'] ? 1 : -1;
-        });
-
-        return response()->json(array_reverse($texts));
+        return response()->json($data);
     }
 }
